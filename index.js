eb3a7299def0ba9321ddbfe4f4063ff1a340afb8
Abenezer <abenezergirma2@gmail.com>
2022-09-18 16:26:05 +0300

    Changed heroku app

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const {google} = require('googleapis');
 const { drive } = require('googleapis/build/src/apis/drive');
 
 const axios = require('axios');
-//const Telegraf = require('telegraf');
-const { Composer } = require('micro-bot');
+const Telegraf = require('telegraf');
+//const { Composer } = require('micro-bot');
 
-//const bot = new Telegraf('5415929895:AAFFslLBQoeMWzDavZA3HYtMX3wVNDKlq9c');
-const bot = new Composer
+const bot = new Telegraf('5415929895:AAFFslLBQoeMWzDavZA3HYtMX3wVNDKlq9c');
+//const bot = new Composer
 
 const RestAPIurl = "https://script.google.com/macros/s/AKfycbwE0kSz-GE06Pgs-4CStv6B1l7JnKnel_NUNpgbwtcT-PyyTSHN/exec";
 
@@ -29,7 +29,7 @@ bot.start((ctx) => {
 })
 
 
-async function listDownloadFIles(){
+async function listDownloadFiles(){
   let res = await axios.get(RestAPIurl)  
 
       result = res.data[0].data;
@@ -48,14 +48,14 @@ Download Link: ${elem.FileUrl}
       return fileDescription;
 }
 
-listDownloadFIles();
+listDownloadFiles();
 
 
 bot.action('Download', (ctx) => {
   var id = ctx.chat.id; 
   ctx.deleteMessage();
 
-  listDownloadFIles().then(async (result) => {
+  listDownloadFiles().then(async (result) => {
     var NumOfResults = result.length;
    
     ctx.telegram.sendMessage(id, "Available Anki Files" +  "\n"  + result, {
@@ -99,17 +99,20 @@ bot.on('document', (ctx) => {
                 "Size: " + String(file.file_size) + "\n"
     };
 
-    fileData = fileDescription.data;
-    console.log(fileData)
+    //fileData = fileDescription.data;
+    //console.log(fileData)
     return ctx.reply("Document received." + "\n" + "Document information: " + "\n" + "\n" + fileData)
 
 }
 );
 
-//bot.launch()
-module.exports = bot
+bot.launch()
+//module.exports = bot
 
 //web: micro-bot -p $PORT => tried to change it to worker dyno => 
+//Please work on this!!!! 
+
+
 //Server ID - aqueous-cove-61595
 //Server URL - https://aqueous-cove-61595.herokuapp.com/
 
